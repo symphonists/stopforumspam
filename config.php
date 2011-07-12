13ab1e111f2a63f8861cd5468559862228e66365
Giel Berkers <info@gielberkers.com>
2011-07-12 15:31:28 +0200

    - added configuration file - added 2 additional methods of spam detection

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Enter the name of your username field:
+$username = 'username';
+
+// Enter the name of your e-mail address field:
+$email = 'e-mail-address';
+
+// Enter the name of the hidden field:
+$hidden = 'hidden-field';
+
+// Enter the name of the timestamp field:
+$timestamp = 'timestamp';
+
